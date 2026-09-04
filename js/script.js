c4b66ff21e4286d3e431653679fc59a45647f7f9
hashglobal25 <hashglobal25@gmail.com>
2026-09-04 08:01:42 +0800

    gtm

--- js/script.js
+++ js/script.js
@@ -13,4 +13,50 @@ jQuery(document).ready(function ($) {
     // Check if Bootstrap JavaScript is loaded.
     console.log('Bootstrap:', typeof bootstrap);
 
+// Tracking code for contactform 7
+
+    // Track when the user starts interacting with a Contact Form 7 form.
+    $(document).on('focusin', '.wpcf7 input, .wpcf7 textarea, .wpcf7 select', function () {
+        window.dataLayer = window.dataLayer || [];
+
+        // Only fire once per page.
+        if (!window.cf7FormStarted) {
+            window.cf7FormStarted = true;
+
+            window.dataLayer.push({
+                event: 'form_started'
+            });
+        }
+    });
+
+    // Track successful Contact Form 7 submissions.
+    document.addEventListener('wpcf7mailsent', function () {
+        window.dataLayer = window.dataLayer || [];
+
+        window.dataLayer.push({
+            event: 'form_submitted'
+        });
+    });
+
+    // Track failed/invalid Contact Form 7 submissions.
+    document.addEventListener('wpcf7invalid', function () {
+        window.dataLayer = window.dataLayer || [];
+
+        window.dataLayer.push({
+            event: 'form_submission_failure'
+        });
+    });
+
+    document.addEventListener('wpcf7mailfailed', function () {
+        window.dataLayer = window.dataLayer || [];
+
+        window.dataLayer.push({
+            event: 'form_submission_failure'
+        });
+    });
+
+
 });
+
+
+
